cfbe4e64c42882183091f07194c0c2c4668f7407
Maruthek1dd <leonardomarusich96@gmail.com>
2025-05-26 20:50:20 -0300

    readme upload

--- main.py
+++ main.py
@@ -1,35 +1,6 @@
 import uuid
 import os
 
-
-"""
----------------------------------------------------------------
-SkyRoute - Sistema de Gestión de Pasajes Aéreos 
----------------------------------------------------------------
-
-Propósito del sistema:
-Este programa permite gestionar la información básica de una agencia de viajes.
-Funciones disponibles:
-- Gestión de clientes
-- Consulta de ventas
-- Visualización de reportes generales
-
-Instrucciones de instalación y ejecución:
-1. Asegurarse de tener Python instalado (versión 3.7 o superior).
-2. Descargar o clonar el archivo 'main.py'.
-3. Ejecutar el programa desde la terminal o consola con el siguiente comando:
-
-    python main.py
-
-
-Integrantes: 
-- Marusich Leonardo 39475993
-- Gonzalez Leonardo 39058972
-- Trinidad Dario Exequiel 36480217
-- Mazzei Juan Cruz 40443228
-- Mancini Pereyra Marcelo Agustín 40110119
-
-"""
 if __name__ == "__main__":
     os.system('cls' if os.name == 'nt' else 'clear')
 
---
+++ readme.md
@@ -0,0 +1,30 @@
+---
+
+## SkyRoute - Sistema de Gestión de Pasajes Aéreos
+
+Propósito del sistema:
+Este programa permite gestionar la información básica de una agencia de viajes.
+Funciones disponibles:
+
+- Gestión de clientes
+- Consulta de ventas
+- Visualización de reportes generales
+
+Instrucciones de instalación y ejecución:
+
+1. Asegurarse de tener Python instalado (versión 3.7 o superior).
+2. Descargar o clonar el archivo 'main.py'.
+3. Ejecutar el programa desde la terminal o consola con el siguiente comando:
+
+   python main.py
+
+Integrantes:
+
+- Marusich Leonardo 39475993
+- Gonzalez Leonardo 39058972
+- Trinidad Dario Exequiel 36480217
+- Mazzei Juan Cruz 40443228
+- Mancini Pereyra Marcelo Agustín 40110119
+
+Link al repositorio:
+https://github.com/Maruthek1dd/skyroute.git
